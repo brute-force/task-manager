26ae21825112fd855171ee21e831438365262b8c
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-18 13:53:16 -0400

    save token in request for log out route handlers

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
         }
 
         req.user = user;
+        req.token = token;
         next();
     } catch (err) {
         res.status(401).send({error: `Please log in. ${err.message}`});
