568d2a8bf9a273c9e22ac21e71519e0b409596fb
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-19 01:26:55 -0400

    use environment variables for key pair if available

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,8 +3,8 @@ const path = require('path');
 const fs = require('fs');
 
 // use rsa keys for jwt
-const keyPrivate  = fs.readFileSync(path.join(__dirname, '../private.key'));
-const keyPublic  = fs.readFileSync(path.join(__dirname, '../public.key'));
+let keyPrivate = process.env.KEY_PRIVATE ? process.env.KEY_PRIVATE : fs.readFileSync(path.join(__dirname, '../private.key'));
+let keyPublic = process.env.KEY_PUBLIC ? process.env.KEY_PUBLIC : fs.readFileSync(path.join(__dirname, '../public.key'));
 
 const signOpts = {
     expiresIn: '1 hour',
