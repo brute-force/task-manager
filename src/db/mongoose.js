21f0d9b845839be22a10a6486cb938e744fbfe86
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-19 01:12:16 -0400

    -move mongodb url components to environment variables -handle srv urls

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 
 (async () => {
     try {
+        let login = '';
+        let params = '';
+        let port = '';
+
+        // handle srv mongodb urls
+        if (process.env.MONGODB_SCHEME === 'mongodb+srv') {
+            login = `${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@`;
+            params = '?retryWrites=true';
+        } else {
+            port = `:${process.env.MONGODB_PORT}`;
+        }
+
+        const url = process.env.MONGODB_SCHEME + '://' + login + process.env.MONGODB_HOST + port + '/' + 
+            process.env.MONGODB_DATABASE + params;
+
         const opts = { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false };
-        await mongoose.connect(`${process.env.MONGODB_SCHEME}://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/task-manager-api`, opts);
+
+        await mongoose.connect(url, opts);
     } catch (err) {
         console.log(`error connecting: ${err.message}`);
     }
