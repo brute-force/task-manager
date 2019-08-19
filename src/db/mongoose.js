35f4061e246ba27d75d71f9710bfbbb229b80e92
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-19 00:28:18 -0400

    use environment variables for mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 (async () => {
     try {
         const opts = { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false };
-        await mongoose.connect('mongodb://localhost:27017/task-manager-api', opts);
+        await mongoose.connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/task-manager-api`, opts);
     } catch (err) {
         console.log(`error connecting: ${err.message}`);
     }
