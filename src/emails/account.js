1e0819f26aafd0c1e009a76ef5f8fc184c159275
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-18 23:31:56 -0400

    email send module

---
+++ src/emails/account.js
@@ -0,0 +1,29 @@
+const sgMail = require('@sendgrid/mail');
+
+const apiKey = process.env.API_KEY_SENDGRID;
+sgMail.setApiKey(apiKey);
+
+const sendEmailWelcome = (email, name) => {
+    sgMail.send({
+        from: 'reuel@panix.com',
+        to: email,
+        subject: `Welcome, ${name}!`,
+        text: 'yes, hello.',
+        html: '<b>yes, hello.</b>'
+    });
+};
+
+const sendEmailCancellation = (email, name) => {
+    sgMail.send({
+        from: 'reuel@panix.com',
+        to: email,
+        subject: `BUH BYE, ${name}!`,
+        text: 'bye.',
+        html: '<b>yes, bye.</b>'
+    });
+};
+
+module.exports = {
+    sendEmailWelcome,
+    sendEmailCancellation
+};
