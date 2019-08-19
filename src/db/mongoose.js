0fc32b400c2a74721f1124195878ba73665e709c
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-08-19 00:49:38 -0400

    move mongodb scheme to environment variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 (async () => {
     try {
         const opts = { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false };
-        await mongoose.connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/task-manager-api`, opts);
+        await mongoose.connect(`${process.env.MONGODB_SCHEME}://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/task-manager-api`, opts);
     } catch (err) {
         console.log(`error connecting: ${err.message}`);
     }
