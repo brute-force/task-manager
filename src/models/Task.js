b9902877f51aa321a35580c6f99267c57780eb49
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-06 12:03:14 -0400

    add required title and date due fields

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,24 +1,32 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const getModel = require('../db/mongoose');
 
 const schemaTask = new mongoose.Schema({
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    isCompleted: {
-        type: Boolean,
-        default: false
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  dateDue: {
+    type: Date,
+    required: true
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 const Task = getModel('Task', schemaTask);
