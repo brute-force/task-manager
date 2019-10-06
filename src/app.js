f97eac84c36d3e7180e975dc8f8ce7569dc18336
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-06 11:56:29 -0400

    add cors support

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 require('./db/mongoose');
 const routerUsers = require('./routers/users');
 const routerTasks = require('./routers/tasks');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routerUsers);
 app.use(routerTasks);
