a8e511e15fce7dabc87f4bf87ba7b4bb6501a5f1
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-06 11:58:21 -0400

    fix linting, increase token timeout from 1 hour to 1 day

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,35 +3,25 @@ const path = require('path');
 const fs = require('fs');
 
 // use rsa keys for jwt
-let keyPrivate = process.env.KEY_PRIVATE ? process.env.KEY_PRIVATE : fs.readFileSync(path.join(__dirname, '../private.key'));
-let keyPublic = process.env.KEY_PUBLIC ? process.env.KEY_PUBLIC : fs.readFileSync(path.join(__dirname, '../public.key'));
+const keyPrivate = process.env.KEY_PRIVATE ? process.env.KEY_PRIVATE : fs.readFileSync(path.join(__dirname, '../private.key'));
+const keyPublic = process.env.KEY_PUBLIC ? process.env.KEY_PUBLIC : fs.readFileSync(path.join(__dirname, '../public.key'));
 
 const signOpts = {
-    expiresIn: '1 hour',
-    algorithm: 'RS256'
+  expiresIn: '1 day',
+  algorithm: 'RS256'
 };
 
 const sign = (id) => {
-    try {
-        const token = jwt.sign({ _id: id },  keyPrivate, signOpts);
-        return token;
-    } catch (err) {
-        throw err;
-    }
+  return jwt.sign({ _id: id }, keyPrivate, signOpts);
 };
 
 const verify = (token) => {
-    try {
-        const decoded = jwt.verify(token, keyPublic);
-        return decoded;
-    } catch (err) {
-        throw err;
-    }
+  return jwt.verify(token, keyPublic);
 };
 
 module.exports = {
-    keyPrivate,
-    keyPublic,
-    sign,
-    verify
+  keyPrivate,
+  keyPublic,
+  sign,
+  verify
 };
