5ccc969ab4b46e5627be7161000932f73f6f7f3a
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-06 12:04:33 -0400

    add 'get all' routes, fix linting

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -3,80 +3,115 @@ const auth = require('../middleware/auth');
 const Task = require('../models/Task');
 
 const routerTasks = new express.Router();
-const optsUpdate = { new: true, runValidators: true };
+// const optsUpdate = { new: true, runValidators: true };
 
 // get tasks
-// params: 
+// params:
 // -isCompleted true/false
 // -limit int
 // -skip int
 // -sortBy field:asc/desc
 routerTasks.get('/tasks', auth, async (req, res) => {
-    // retrieve via search by owner
-    // const tasks = await Task.find({ owner: req.user._id }).catch((err) => res.status(500).send());
+  // retrieve via search by owner
+  // const tasks = await Task.find({ owner: req.user._id }).catch((err) => res.status(500).send());
 
-    // retrieve via populating user tasks
-    const match = req.query.isCompleted ? { isCompleted: (req.query.isCompleted === 'true') } : {};
+  // retrieve via populating user tasks
+  const match = req.query.isCompleted ? { isCompleted: (req.query.isCompleted === 'true') } : {};
 
-    // query options
-    const options = { 
-        limit: parseInt(req.query.limit), 
-        skip: parseInt(req.query.skip),
-        sort: {}
-    };
+  // query options
+  const options = {
+    limit: parseInt(req.query.limit),
+    skip: parseInt(req.query.skip),
+    sort: {}
+  };
 
-    // parse sort
-    if (req.query.sortBy) {
-        const sortComponents = req.query.sortBy.split(':');
-        options.sort[sortComponents[0]] = (sortComponents[1].toLowerCase() === 'desc') ? -1 : 1
-    }
+  // parse sort
+  if (req.query.sortBy) {
+    const sortComponents = req.query.sortBy.split(':');
+    options.sort[sortComponents[0]] = (sortComponents[1].toLowerCase() === 'desc') ? -1 : 1;
+  }
 
-    await req.user.populate({ path: 'tasks', match, options }).execPopulate().catch((err) => res.status(500).send());
-    res.send(req.user.tasks);
+  await req.user.populate({ path: 'tasks', match, options }).execPopulate().catch((err) => {
+    console.log(err);
+    res.status(500).send();
+  });
+  res.send(req.user.tasks);
+});
+
+// get all tasks
+routerTasks.get('/tasks/all', auth, async (req, res) => {
+  const tasks = await Task.find({ }).populate('owner', 'name email').exec().catch((err) => {
+    console.log(err);
+    res.status(500).send();
+  });
+
+  if (tasks) {
+    res.send(tasks);
+  } else {
+    res.status(404).send();
+  }
 });
 
 // get task by id
 routerTasks.get('/tasks/:id', auth, async (req, res) => {
-    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id }).catch((err) => res.status(500).send());
-    task ? res.send(task) : res.status(404).send();
+  const task = await Task.findOne({ _id: req.params.id, owner: req.user._id }).catch((err) => {
+    console.log(err);
+    res.status(500).send();
+  });
+
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send();
+  }
 });
 
 // create task
 routerTasks.post('/tasks', auth, async (req, res) => {
-    const task = new Task({ ...req.body, owner: req.user._id });
-    await task.save()
-        .then((taskSaved) => res.status(201).send(taskSaved))
-        .catch((err) => res.status(400).send(`error saving task: ${err.message}`));
+  const task = new Task({ ...req.body, owner: req.user._id });
+  await task.save()
+    .then((taskSaved) => res.status(201).send(taskSaved))
+    .catch((err) => res.status(400).send(`error saving task: ${err.message}`));
 });
 
 // update task
 routerTasks.patch('/tasks/:id', auth, async (req, res) => {
-    const updates = Object.keys(req.body);
-    const updateAllowables = ['description', 'isCompleted'];
-    const doUpdate = updates.every((update) => updateAllowables.includes(update));
+  const updates = Object.keys(req.body);
+  const updateAllowables = ['title', 'description', 'dateDue', 'isCompleted'];
+  const doUpdate = updates.every((update) => updateAllowables.includes(update));
 
-    if (doUpdate) {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id }).catch((err) => res.status(500).send());
+  if (doUpdate) {
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id }).catch((err) => {
+      console.log(err);
+      res.status(500).send();
+    });
 
-        if (!task) {
-            return res.status(404).send();
-        }
-        
-        updates.forEach((update) => task[update] = req.body[update]);
-
-        await task.save()
-            .then((taskSaved) => res.send(taskSaved))
-            .catch((err) => res.status(400).send(`error updating task: ${err.message}`));
-    } else {
-        res.status(400).send('invalid task update');
+    if (!task) {
+      return res.status(404).send();
     }
+
+    updates.forEach((update) => {
+      task[update] = req.body[update];
+    });
+
+    await task.save()
+      .then((taskSaved) => res.send(taskSaved))
+      .catch((err) => res.status(400).send(`error updating task: ${err.message}`));
+  } else {
+    res.status(400).send('invalid task update');
+  }
 });
 
 // delete task
 routerTasks.delete('/tasks/:id', auth, async (req, res) => {
-    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
-        .catch((err) => res.status(500).send(err));
-    task ? res.send(task) : res.status(400).send();
+  const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+    .catch((err) => res.status(500).send(err));
+
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(400).send();
+  }
 });
 
 module.exports = routerTasks;
--- src/routers/users.js
+++ src/routers/users.js
@@ -6,136 +6,158 @@ const sharp = require('sharp');
 const { sendEmailWelcome, sendEmailCancellation } = require('../emails/account');
 
 const routerUsers = new express.Router();
-const optsUpdate = { new: true, runValidators: true };
+// const optsUpdate = { new: true, runValidators: true };
 
 // log in user
 routerUsers.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token = await user.generateAuthToken();
-        res.send({ user: user.getPublicProfile(), token });
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user: user.getPublicProfile(), token });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
 
 // log out user
 routerUsers.get('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
-        await req.user.save();
-        res.send();
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 // log out user (all sessions)
 routerUsers.get('/users/logout/all', auth, async (req, res) => {
-    try {
-        req.user.tokens = [];
-        await req.user.save();
-        res.send();
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 // get user me
 routerUsers.get('/users/me', auth, async (req, res) => {
-    res.send({ user: req.user.getPublicProfile(), token: req.token });
+  res.send({ user: req.user.getPublicProfile(), token: req.token });
+});
+
+// get all users
+routerUsers.get('/users/all', auth, async (req, res) => {
+  const users = await User.find({ }).catch((err) => {
+    console.log(err);
+    res.status(500).send();
+  });
+
+  if (users) {
+    res.send(users.map((user) => user.getPublicProfile()));
+  } else {
+    res.status(404).send();
+  }
 });
 
 // get user by id
 routerUsers.get('/users/:id', auth, async (req, res) => {
-    const user = await User.findById(req.params.id).catch((err) => res.status(500).send());
-    user ? res.send(user) : res.status(404).send();
+  const user = await User.findById(req.params.id).catch((err) => {
+    console.log(err);
+    res.status(500).send();
+  });
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.status(404).send();
+  }
 });
 
 // create user
 routerUsers.post('/users', async (req, res) => {
-    const user = new User(req.body);
+  const user = new User(req.body);
 
-    try {
-        await user.save();
-        sendEmailWelcome(user.email, user.name);
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user: user.getPublicProfile(), token })
-    } catch (err) {
-        res.status(400).send(`error saving user: ${err.message}`);
-    }
+  try {
+    await user.save();
+    sendEmailWelcome(user.email, user.name);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user: user.getPublicProfile(), token });
+  } catch (err) {
+    res.status(400).send(`error saving user: ${err.message}`);
+  }
 });
 
 // update user
 routerUsers.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body);
-    const updateAllowables = ['name', 'email', 'password', 'age'];
-    const doUpdate = updates.every((update) => updateAllowables.includes(update));
+  const updates = Object.keys(req.body);
+  const updateAllowables = ['name', 'email', 'password', 'age'];
+  const doUpdate = updates.every((update) => updateAllowables.includes(update));
 
-    if (doUpdate) {
-        updates.forEach((update) => req.user[update] = req.body[update]);
+  if (doUpdate) {
+    updates.forEach((update) => {
+      req.user[update] = req.body[update];
+    });
 
-        await req.user.save()
-            .then((userSaved) => res.send(userSaved.getPublicProfile()))
-            .catch((err) => res.status(400).send(`error updating user: ${err.message}`));
-    } else {
-        res.status(400).send('invalid user update');
-    }
+    await req.user.save()
+      .then((userSaved) => res.send(userSaved.getPublicProfile()))
+      .catch((err) => res.status(400).send(`error updating user: ${err.message}`));
+  } else {
+    res.status(400).send('invalid user update');
+  }
 });
 
 // delete yoself
 routerUsers.delete('/users/me', auth, async (req, res) => {
-    await req.user.remove().catch((err) => res.status(500).send(err));
-    sendEmailCancellation(req.user.email, req.user.name);
-    res.send(req.user);
+  await req.user.remove().catch((err) => res.status(500).send(err));
+  sendEmailCancellation(req.user.email, req.user.name);
+  res.send(req.user);
 });
 
 // upload options
 const upload = multer({
-    // dest: 'avatars',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, filename, callback) {
-        // filter filename extensions
-        if (!/\.(jpg|jpeg|png)$/.test(filename.originalname.toLowerCase())) {
-            return callback(new Error('file must be a jpg|jpeg|png'));
-        }
-        
-        callback(null, true);
+  // dest: 'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter (req, filename, callback) {
+    // filter filename extensions
+    if (!/\.(jpg|jpeg|png)$/.test(filename.originalname.toLowerCase())) {
+      return callback(new Error('file must be a jpg|jpeg|png'));
     }
+
+    callback(null, true);
+  }
 });
 
 // user avatar upload
 routerUsers.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const crop = { width: 100, height: 100 };
+  const crop = { width: 100, height: 100 };
 
-    const buffer = await sharp(req.file.buffer).resize(crop).png().toBuffer();
-    req.user.avatar = buffer;
+  const buffer = await sharp(req.file.buffer).resize(crop).png().toBuffer();
+  req.user.avatar = buffer;
 
-    await req.user.save();
-    res.send();
+  await req.user.save();
+  res.send();
 }, (err, req, res, next) => {
-    res.status(400).send({ error: err.message })
+  res.status(400).send({ error: err.message });
 });
 
 // user avatar lookup by id
 routerUsers.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
 
-    try {
-        const user = await User.findById(req.params.id);
-
-        if (!user || !user.avatar) {
-            throw new Error ('user or user avatar not found')
-        }
-
-        res.contentType('image/png');
-        res.set("Content-Disposition", "inline;");
-        res.send(user.avatar);
-    } catch (err) {
-        res.status(404).send(err.message);
+    if (!user || !user.avatar) {
+      throw new Error('user or user avatar not found');
     }
-});
 
+    res.contentType('image/png');
+    res.set('Content-Disposition', 'inline;');
+    res.send(user.avatar);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+});
 
 module.exports = routerUsers;
