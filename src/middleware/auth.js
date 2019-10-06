3a2d118ee5559f3b7a1349aa2952bc6f959a0d14
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-06 11:57:13 -0400

    fix linting errors, remove unused moment import

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,22 @@
-const jwt = require('jsonwebtoken');
 const utilsJWT = require('../utils/jwt');
 const User = require('../models/User');
 
 const auth = async (req, res, next) => {
-    try {
-        const token = req.header('Authorization').replace(/^Bearer /, '');
-        const decoded = utilsJWT.verify(token);
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
+  try {
+    const token = req.header('Authorization').replace(/^Bearer /, '');
+    const decoded = utilsJWT.verify(token);
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
-        if (!user) {
-            throw new Error('Authentication error.');
-        }
-
-        req.user = user;
-        req.token = token;
-        next();
-    } catch (err) {
-        res.status(401).send({error: `Please log in. ${err.message}`});
+    if (!user) {
+      throw new Error('Authentication error.');
     }
+
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (err) {
+    res.status(401).send({ error: `Please log in. ${err.message}` });
+  }
 };
 
 module.exports = auth;
